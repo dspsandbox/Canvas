2a64ade84bae7c7ce4e1b39700d1b806da5786e0
Pau Gomez K <paugomezkabelka@gmail.com>
2019-07-29 10:56:21 +0200

    clk 250 Mhz -> 180 deg

--- Python/Examples/Example1/Output/VHDL/NET2FPGA_base_DSP_core.vhd
+++ Python/Examples/Example1/Output/VHDL/NET2FPGA_base_DSP_core.vhd
@@ -24,7 +24,7 @@ use xil_defaultlib.all;
 entity NET2FPGA_base_DSP_core is
 	Generic(PORT_WIDTH : integer := 32;
 			CONSTANT_REGISTER_DEPTH : integer := 8);
-    Port ( clk125 : in STD_LOGIC;
+    Port ( clk : in STD_LOGIC;
            regAddr : in STD_LOGIC_VECTOR (31 downto 0);
            regVal : in STD_LOGIC_VECTOR (31 downto 0);
            regWrtEn : in STD_LOGIC;
@@ -44,6 +44,13 @@ type constantRegister1BitType is array (((2**CONSTANT_REGISTER_DEPTH)-1) downto 
 
 signal constantRegister32Bit :  constantRegister32BitType:= (others=>(others=>'0'));
 signal constantRegister1Bit :  constantRegister1BitType:= (others=>'0');
+signal adc1_reg : STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0):= ((PORT_WIDTH-1)=>'1',others=>'0');
+signal adc2_reg : STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0):= ((PORT_WIDTH-1)=>'1',others=>'0');
+signal dac1_reg : STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0):= ((PORT_WIDTH-1)=>'1',others=>'0');
+signal dac2_reg : STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0):= ((PORT_WIDTH-1)=>'1',others=>'0');
+signal digitalIn_reg : STD_LOGIC_VECTOR (7 downto 0):= (others=>'0');
+signal digitalOut_reg : STD_LOGIC_VECTOR (7 downto 0):= (others=>'0');
+signal led_reg : STD_LOGIC_VECTOR (7 downto 0):= (others=>'0');
 
 ---------------------------------------------------------------------------------------------------
 --NET2FPGA SIGNALS START
@@ -57,13 +64,13 @@ signal N005 : STD_LOGIC := '0';
 ---------------------------------------------------------------------------------------------------
 
 begin
-	process(clk125)
+	process(clk)
 	variable regAddr_var : integer :=0;
 	variable regVal32Bit_var : STD_LOGIC_VECTOR ((PORT_WIDTH-1) downto 0):= (others=>'0');
 	variable regVal1Bit_var : STD_LOGIC:='0';
 	variable regType: STD_LOGIC := '0';  --   regType=1 -> 32bit   regType=0 -> 1 bit
 	begin
-		if rising_edge(clk125) then
+		if rising_edge(clk) then
 		--Parse communication for setting constants
 			if regWrtEn='1' then
 				regType:= regAddr(31);
@@ -81,37 +88,45 @@ begin
 		end if;
 	end process;
 
+--port <-> register assignements
+adc1_reg<=adc1;
+adc2_reg<=adc2;
+dac1<=dac1_reg;
+dac2<=dac1_reg;
+digitalIn_reg<=digitalIn;
+digitalOut<=digitalOut_reg;
+led<=led_reg;
 ---------------------------------------------------------------------------------------------------
 --NET2FPGA INSTANTIATIONS AND BEHAVIORAL ASSIGNEMENTS START
 --X_NET2FPGA_32Bit_DAC1_0:
-dac1<=N000;
-
---X_NET2FPGA_32Bit_const_0:
-N000<=constantRegister32Bit(0);
-
---X_NET2FPGA_32Bit_DAC2_0:
-dac2<=N001;
-
-X_NET2FPGA_32Bit_add_0 : entity xil_defaultlib.NET2FPGA_32Bit_add
-port map(clk=>clk125,reset=>N003,dataInA=>N002,dataInB=>N000,dataOut=>N001);
+dac1_reg<=N000;
 
 --X_NET2FPGA_32Bit_ADC1_0:
-N002<=adc1;
+N000<=adc1_reg;
+
+--X_NET2FPGA_32Bit_DAC2_0:
+dac2_reg<=N001;
+
+X_NET2FPGA_32Bit_add_0 : entity xil_defaultlib.NET2FPGA_32Bit_add
+port map(clk=>clk,reset=>N003,dataInA=>N000,dataInB=>N002,dataOut=>N001);
+
+--X_NET2FPGA_32Bit_const_0:
+N002<=constantRegister32Bit(0);
 
 --X_NET2FPGA_1Bit_in0_0:
-N003<=digitalIn(0);
+N003<=digitalIn_reg(0);
 
 --X_NET2FPGA_1Bit_LED0_0:
-led(0)<=N003;
+led_reg(0)<=N003;
 
 --X_NET2FPGA_1Bit_LED1_0:
-led(1)<=N004;
+led_reg(1)<=N004;
 
 --X_NET2FPGA_1Bit_in1_0:
-N004<=digitalIn(1);
+N004<=digitalIn_reg(1);
 
 --X_NET2FPGA_1Bit_LED2_0:
-led(2)<=N005;
+led_reg(2)<=N005;
 
 --X_NET2FPGA_1Bit_const_0:
 N005<=constantRegister1Bit(0);
