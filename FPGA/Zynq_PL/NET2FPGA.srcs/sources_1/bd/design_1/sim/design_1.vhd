f5249ba3c8129e8f6dadd1cda6737a58aef37445
Pau Gomez K <paugomezkabelka@gmail.com>
2019-07-29 11:16:06 +0200

    +90deg for 250 Mhz clk

--- FPGA/Zynq_PL/NET2FPGA.srcs/sources_1/bd/design_1/sim/design_1.vhd
+++ FPGA/Zynq_PL/NET2FPGA.srcs/sources_1/bd/design_1/sim/design_1.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
---Date        : Mon Jul 29 11:07:50 2019
+--Date        : Mon Jul 29 11:14:57 2019
 --Host        : PC1091 running 64-bit major release  (build 9200)
 --Command     : generate_target design_1.bd
 --Design      : design_1
