ea7cdcad43f34cbc707368a66741d8f411134611
Pau Gomez <pgomez@icfo.net>
2019-09-27 12:06:19 +0200

    +30 deg input phase

---
+++ GUI/Python/test/VHDL/DSP_core.vhd
@@ -0,0 +1,133 @@
+----------------------------------------------------------------------------------
+--DSP_core module
+
+--MIT License
+--Copyright (c) 2019 DSPsandbox (Pau Gomez pau.gomez@dspsandbox.org)
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+library xil_defaultlib;
+use xil_defaultlib.all;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity DSP_core is
+	Generic(PORT_WIDTH : integer := 32;
+			CONSTANT_REGISTER_DEPTH : integer := 8);
+    Port ( clk : in STD_LOGIC;
+           regAddr : in STD_LOGIC_VECTOR (31 downto 0);
+           regVal : in STD_LOGIC_VECTOR (31 downto 0);
+           regWrtEn : in STD_LOGIC;
+           adc1 : in STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0);
+           adc2 : in STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0);
+           digitalIn : in STD_LOGIC_VECTOR (7 downto 0);
+           dac1 : out STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0);
+           dac2 : out STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0);
+           digitalOut : out STD_LOGIC_VECTOR (7 downto 0);
+           led : out STD_LOGIC_VECTOR (7 downto 0));
+end DSP_core;
+
+architecture Behavioral of DSP_core is
+
+type x32ConstRegType is array (((2**CONSTANT_REGISTER_DEPTH)-1) downto 0) of STD_LOGIC_VECTOR((PORT_WIDTH-1) downto 0);
+type x1ConstRegType is array (((2**CONSTANT_REGISTER_DEPTH)-1) downto 0) of STD_LOGIC;
+
+signal x32ConstReg :  x32ConstRegType:= (others=>(others=>'0'));
+signal x1ConstReg :  x1ConstRegType:= (others=>'0');
+signal adc1_signal : STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0):= (others=>'0');
+signal adc2_signal : STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0):= (others=>'0');
+signal dac1_signal : STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0):= (others=>'0');
+signal dac2_signal : STD_LOGIC_VECTOR (PORT_WIDTH-1 downto 0):= (others=>'0');
+signal digitalIn_signal : STD_LOGIC_VECTOR (7 downto 0):= (others=>'0');
+signal digitalOut_signal : STD_LOGIC_VECTOR (7 downto 0):= (others=>'0');
+signal led_signal : STD_LOGIC_VECTOR (7 downto 0):= (others=>'0');
+
+---------------------------------------------------------------------------------------------------
+--AUTO GENERATED SIGNALS START
+signal N000 : STD_LOGIC_VECTOR((PORT_WIDTH-1) downto 0) := ((PORT_WIDTH-1)=>'1',others=>'0');
+signal N001 : STD_LOGIC_VECTOR((PORT_WIDTH-1) downto 0) := ((PORT_WIDTH-1)=>'1',others=>'0');
+signal N002 : STD_LOGIC_VECTOR((PORT_WIDTH-1) downto 0) := ((PORT_WIDTH-1)=>'1',others=>'0');
+signal N003 : STD_LOGIC := '0';
+signal N004 : STD_LOGIC_VECTOR((PORT_WIDTH-1) downto 0) := ((PORT_WIDTH-1)=>'1',others=>'0');
+signal N005 : STD_LOGIC_VECTOR((PORT_WIDTH-1) downto 0) := ((PORT_WIDTH-1)=>'1',others=>'0');
+signal N006 : STD_LOGIC := '0';
+--AUTO GENERATED SIGNALS END
+---------------------------------------------------------------------------------------------------
+
+
+begin
+	process(clk)
+	variable regAddr_var : integer :=0;
+	variable x32RegVal_var : STD_LOGIC_VECTOR ((PORT_WIDTH-1) downto 0):= (others=>'0');
+	variable x1RegVal_var : STD_LOGIC:='0';
+	variable regType: STD_LOGIC := '0';  --   regType=1 -> 32bit   regType=0 -> 1 bit
+	begin
+		if rising_edge(clk) then
+		--Parse communication for setting constants
+			if regWrtEn='1' then
+				regType:= regAddr(31);
+				regAddr_var:=to_integer(unsigned(regAddr((CONSTANT_REGISTER_DEPTH-1) downto 0)));
+				x32RegVal_var:= regVal;
+				x1RegVal_var:= regVal(0);
+
+				if regType='1' then
+					x32ConstReg(regAddr_var)<=x32RegVal_var;
+				else
+					x1ConstReg(regAddr_var)<=x1RegVal_var;
+				end if;	
+			end if;
+		end if;	
+	end process;
+
+	-- in/out port <-> signal assignements	
+    adc1_signal<=adc1;
+    adc2_signal<=adc2;
+    dac1<=dac1_signal;
+    dac2<=dac2_signal;
+    digitalIn_signal<=digitalIn;
+    digitalOut<=digitalOut_signal;
+    led<=led_signal;	
+    
+---------------------------------------------------------------------------------------------------
+--AUTO GENERATED INSTANTIATIONS AND BEHAVIORAL ASSIGNEMENTS START
+--x32_dac1_0:
+dac1_signal<=N000;
+
+x32_add_0 : entity xil_defaultlib.x32_add
+port map(clk=>clk,reset=>N003,dataIn0=>N001,dataIn1=>N002,dataOut=>N000);
+
+x32_add_1 : entity xil_defaultlib.x32_add
+port map(clk=>clk,reset=>N006,dataIn0=>N004,dataIn1=>N005,dataOut=>N001);
+
+--x32_adc1_0:
+N004<=adc1_signal;
+
+--x32_adc2_0:
+N005<=adc2_signal;
+
+--x1_const_0:
+N006<=x1ConstReg(0);
+
+--x32_const_0:
+N002<=x32ConstReg(0);
+
+--x1_const_1:
+N003<=x1ConstReg(1);
+
+--x32_dac2_0:
+dac2_signal<=N000;
+
+--AUTO GENERATED INSTANTIATIONS AND BEHAVIORAL ASSIGNEMENTS END
+---------------------------------------------------------------------------------------------------
+
+end Behavioral;
---
+++ GUI/Python/test/Zynq_7010/Zynq_PS/constantsLoader.c
@@ -0,0 +1,95 @@
+/*********************************************************************
+ Canvas constantsLoader.c
+
+ MIT License
+ Copyright (c) 2019 DSPsandbox (Pau Gomez pau.gomez@dspsandbox.org)
+**********************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string.h>
+
+#define on_error(...) { fprintf(stderr, __VA_ARGS__); fflush(stderr); exit(1); }
+
+//MEMORY ADDR
+#define GPIO_ADDR_regAddr  0x41200000
+#define GPIO_ADDR_regVal   0x41220000
+#define GPIO_ADDR_regWrtEn 0x41210000
+
+#define FILE_NAME_CONST_32BIT "/home/Canvas/x32Const.txt"
+#define FILE_NAME_CONST_1BIT "/home/Canvas/x1Const.txt"
+
+char *name = "/dev/mem";
+int fd;
+
+uint32_t *regAddr_;
+int32_t *regVal_;
+uint32_t *regWrtEn_;
+
+uint32_t regType;
+FILE * f;
+
+int main () {
+    //Memory mapping
+    if((fd = open(name, O_RDWR)) < 0) {
+                    printf("CANNOT OPEN MEMORY FILE\n");
+                    return 0;
+                }
+    
+    regAddr_ = mmap(NULL, sysconf(_SC_PAGESIZE), 
+                PROT_READ|PROT_WRITE, MAP_SHARED, fd, GPIO_ADDR_regAddr);
+                
+    regVal_ = mmap(NULL, sysconf(_SC_PAGESIZE), 
+                PROT_READ|PROT_WRITE, MAP_SHARED, fd, GPIO_ADDR_regVal);
+                
+    regWrtEn_ = mmap(NULL, sysconf(_SC_PAGESIZE), 
+                PROT_READ|PROT_WRITE, MAP_SHARED, fd, GPIO_ADDR_regWrtEn);
+
+
+    // 1Bit 
+    regType=0; // regType=0 -> 1Bit 
+    f=fopen(FILE_NAME_CONST_1BIT,"r");
+    for (int i=0; i<256; i++){
+        uint32_t regAddr;
+        int32_t regVal;
+        fscanf(f,"%d   %d\r\n",&regAddr,&regVal);
+     
+        regAddr=regAddr|(regType<<31);
+        *regAddr_=regAddr;
+        *regVal_=regVal;
+        *regWrtEn_=1;
+        *regWrtEn_=0;
+        if (regVal!=0){
+            printf("1Bit -> Addr: %d   Val: %d\r\n",regAddr,regVal);
+        }
+    }
+    printf("1Bit -> Addr: others   Val: 0 \r\n");
+    fclose(f);
+    printf("------------------------------------\r\n");
+    
+    // 32Bit
+    regType=1; // regType=1 -> 32Bit 
+    f=fopen(FILE_NAME_CONST_32BIT,"r");
+    for (int i=0; i<256; i++){
+        uint32_t regAddr;
+        int32_t regVal;
+        fscanf(f,"%d   %d\r\n",&regAddr,&regVal);
+     
+        regAddr=regAddr|(regType<<31);
+        *regAddr_=regAddr;
+        *regVal_=regVal;
+        *regWrtEn_=1;
+        *regWrtEn_=0;
+        if (regVal!=0){
+            printf("32Bit -> Addr: %d   Val: %d\r\n",regAddr&(~(1<<31)),regVal);
+        }
+    }
+    printf("32Bit -> Addr: others   Val: 0 \r\n");
+    fclose(f);   
+                
+    return 0;
+}
