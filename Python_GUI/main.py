cba329394987cc30479c19b3d90805b809c2c95f
Pau <pgomez@QSWS10.quside.net>
2019-10-08 19:55:12 +0200

    Update icon

--- Python_GUI/main.py
+++ Python_GUI/main.py
@@ -51,7 +51,7 @@ class CanvasApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.projectName=""
         self.projectDirPath=""
         self.projectFilePath=""
-        self.setWindowIcon(QtGui.QIcon(os.path.join(pathResoures,"GUI","icon.png")))
+        self.setWindowIcon(QtGui.QIcon(os.path.join(pathResoures,"GUI","iconWhiteBackground.png")))
         #Timer
         self.timerSynthImpl = QtCore.QTimer()
         #SSH objects
